6852e9414f190c068d72fd97febf6347ffe849ad
Artalus <artalus-mail@yandex.ru>
2017-09-07 21:02:56 +0300

    adding and geting balls in phys system

--- physics/phys_system.cpp
+++ physics/phys_system.cpp
@@ -1,17 +1,33 @@
 #include "phys_system.h"
+#include <string>
 
-int phys_system::add(const ball &ball) {
-	return 1;
+uint64_t phys_system::add(const vec2 &pos) {
+	ball_wrapper w(pos);
+	auto id = w.id;
+	const auto place = std::lower_bound(begin(balls_), end(balls_), id,
+		[](auto &&lhs, auto rhs) {
+		return lhs.id <= rhs;
+	});
+	balls_.emplace(place, std::move(w));
+	return id;
 }
 
 size_t phys_system::size() const {
-	return 0;
+	return balls_.size();
 }
 
 void phys_system::simulate(std::chrono::milliseconds delta) {
 }
 
-const ball & phys_system::get(int bm) {
-	static ball b{{20,-30}};
-	return b;
+const ball & phys_system::get(uint64_t id) {
+	auto it = lower_bound(begin(balls_), end(balls_), id,
+		[](auto &&lhs, auto rhs) {
+		return lhs.id < rhs;
+	});
+	if (it == end(balls_))
+		throw std::runtime_error("couldn't find ball with id " + std::to_string(id));
+	return it->get();
 }
+
+
+uint64_t phys_system::ball_wrapper::guid_ = 1;
--- physics/phys_system.h
+++ physics/phys_system.h
@@ -1,16 +1,46 @@
 #pragma once
-#include "ball.h"
 #include <chrono>
+#include <memory>
+
+#include "ball.h"
 
 
 class phys_system
 {
+	class ball_wrapper {
+		static uint64_t guid_;
+		std::unique_ptr<ball> b_;
+	public:
+		uint64_t id;
+
+		explicit ball_wrapper(vec2 pos = {})
+			: b_{std::make_unique<ball>(pos)}
+			, id{guid_++}
+		{
+			if (guid_ == 0)
+				throw std::runtime_error("guid overflow");
+		}
+
+		ball_wrapper(const ball_wrapper&) = delete;
+		ball_wrapper(ball_wrapper&&) = default;
+		ball_wrapper& operator=(const ball_wrapper&) = delete;
+		ball_wrapper& operator=(ball_wrapper&&) = default;
+
+		const ball& get() const {
+			return *b_;
+		}
+		bool operator<(const ball_wrapper &rhs) const {
+			return id < rhs.id;
+		}
+	};
+
+	std::vector<ball_wrapper> balls_;
+
 public:
 	phys_system() = default;
 	~phys_system() = default;
-	int add(const ball & ball);
+	uint64_t add(const vec2 &pos);
 	size_t size() const;
 	void simulate(std::chrono::milliseconds delta);
-	const ball& get(int bm);
+	const ball& get(uint64_t bm);
 };
-
